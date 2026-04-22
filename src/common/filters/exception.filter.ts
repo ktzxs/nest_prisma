9d8e66178d3c42d57e5621d7810ef9be9fdc5361
ktzxs <lucasalvesdacosta070@gmail.com>
2026-04-22 15:50:23 -0300

    tirado o comentario do exception 👍

--- src/common/filters/exception.filter.ts
+++ src/common/filters/exception.filter.ts
@@ -1,19 +1,16 @@
-// import { from } from "rxjs";
-// import  {
-//     ArgumentsHost,
-//     Catch,
-//     ExceptionFilter,
-//     HttpException
-// } from '@nestjs/common';
-// import { Request, Response } from "@nestjs/common";
-// import path from "path";
-// import { timestamp } from "rxjs";
+import {
+    ArgumentsHost,
+    Catch,
+    ExceptionFilter,
+    HttpException,
+} from '@nestjs/common'
+import { Request, Response } from 'express';
 
-// @Catch(HttpException)
-// export class ApiExecptionFilter implements ExceptionFilter {
-//     catch(exception: any, host: ArgumentsHost) {
-//         const ctx = host.switchToHttp();
-//         const response = ctx.getResponse<Response>();
-//         const request = ctx.getRequest<Request>();
-//     }
-// }
+@Catch(HttpException)
+export class ApiException implements ExceptionFilter {
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp()
+        const response = ctx.getResponse<Response>()
+        const request = ctx.getRequest<Request>()
+    }
+}
